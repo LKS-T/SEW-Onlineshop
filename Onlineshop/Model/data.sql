b93fcb617f8f69f588cdd4fcb43a0d9fa1d202a8
LKS-T <136821134+LKS-T@users.noreply.github.com>
2026-02-18 22:15:11 +0100

    Create data.sql

---
+++ Onlineshop/Model/data.sql
@@ -0,0 +1,11 @@
+use onlineshopdb;
+INSERT INTO PRODUCTS(NAME, DESCRIPTION, PRICE, IMAGE_URL) 
+VALUES ('Tacker','Des mocht sochan fest',4.99,'https://imgs.search.brave.com/HMjwQGsv2Uvy2pr0uZWYkt2xuY1WU6ZvPdl9jx_l-AU/rs:fit:0:180:1:0/g:ce/aHR0cHM6Ly9pLmVi/YXlpbWcuY29tL2lt/YWdlcy9nL2l3a0FB/T1N3WlZ0bkExZXgv/cy1sMTQwLmpwZw');
+INSERT INTO PRODUCTS(NAME, DESCRIPTION, PRICE, IMAGE_URL)
+VALUES ('Iphone','Des mim Opfe',9999.99,'https://imgs.search.brave.com/DCd9z0sSKMUHpCarW6cUllE84jj02hZRgn3lOsBRbTo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/YmFzaWN0aGlua2lu/Zy5kZS9ibG9nL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDIyLzA5/LzAxLWdhZGdldHMt/dGVjaG5zaWNoZS1n/ZXJhdGUuanBlZw');
+INSERT INTO PRODUCTS(NAME, DESCRIPTION, PRICE, IMAGE_URL)
+VALUES ('Laptop','Appel not included',1.99,'https://imgs.search.brave.com/xKKKaRsNvjGBSTb8mSa7b12RnA0BC8HsOwPv-Lp5Wuc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMucGV4ZWxzLmNv/bS9waG90b3MvMTgx/MDUvcGV4ZWxzLXBo/b3RvLmpwZz9hdXRv/PWNvbXByZXNzJmNz/PXRpbnlzcmdiJmRw/cj0xJnc9NTAw');
+INSERT INTO PRODUCTS(NAME, DESCRIPTION, PRICE, IMAGE_URL)
+VALUES ('DVD','sogar mit am füm drauf',19.99,'https://imgs.search.brave.com/emKFMRqsK4l1fVJwbNdB-UdciCK6YoyX8FnSo6WZ9As/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLmVi/YXlpbWcuY29tL2lt/YWdlcy9nLzdyY0FB/T1N3RmYxa1NmcDcv/cy1sMjI1LmpwZw');
+INSERT INTO PRODUCTS(NAME, DESCRIPTION, PRICE, IMAGE_URL)
+VALUES ('Auto','macht brumm brumm',0.50,'https://imgs.search.brave.com/DTYonrJwodeXiwTIYas-vViwS7MIUdi1XjfchoiQqQE/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNS8w/MS8xOS8xMy81MS9j/YXItNjA0MDE5XzY0/MC5qcGc');
