6095f281229a92c635fee1b9606095d807fc8b93
LKS-T <136821134+LKS-T@users.noreply.github.com>
2026-02-18 22:07:49 +0100

    Update ICheckoutHandler.cs

--- Onlineshop/WebUi/ICheckoutHandler.cs
+++ Onlineshop/WebUi/ICheckoutHandler.cs
@@ -7,6 +7,5 @@ public interface ICheckoutHandler
     Dictionary<Product,int> GetProducts();
     void AddProduct(Product p, int quantity);
     void RemoveProduct(Product p);
-    void UpdateQuantity(Product p, int quantity);
     Task CheckoutAsync();
 }
